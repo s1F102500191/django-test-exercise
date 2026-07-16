1074c8266d0f04396ef7b6082494d1a08df65577
aotoharukan <s1f102302775@iniad.org>
2026-07-16 14:10:34 +0900

    現在の機能に合わせてテストを追加、修正

--- todo/tests.py
+++ todo/tests.py
@@ -1,11 +1,13 @@
-from django.test import TestCase, Client
-from django.utils import timezone
 from datetime import datetime
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
 from todo.models import Task
-from todo import views as todo_views
 
 
-# Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
@@ -13,180 +15,440 @@ class SampleTestCase(TestCase):
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
-        due = timezone.make_aware(datetime(2026, 6, 24, 23, 59, 59))
-        task = Task(title='task1', due_at=due)
-        task.save()
+        due = timezone.make_aware(
+            datetime(2026, 6, 24, 23, 59, 59)
+        )
 
-        task = Task.objects.get(pk=task.pk)
-        self.assertEqual(task.title, 'task1')
-        self.assertFalse(task.completed)
-        self.assertEqual(task.due_at, due)
+        task = Task.objects.create(
+            title='task1',
+            due_at=due
+        )
+
+        saved_task = Task.objects.get(pk=task.pk)
+
+        self.assertEqual(saved_task.title, 'task1')
+        self.assertFalse(saved_task.completed)
+        self.assertEqual(saved_task.due_at, due)
 
     def test_create_task2(self):
-        task = Task(title='task2')
-        task.save()
+        task = Task.objects.create(
+            title='task2'
+        )
 
-        task = Task.objects.get(pk=task.pk)
-        self.assertEqual(task.title, 'task2')
-        self.assertFalse(task.completed)
-        self.assertEqual(task.due_at, None)
+        saved_task = Task.objects.get(pk=task.pk)
+
+        self.assertEqual(saved_task.title, 'task2')
+        self.assertFalse(saved_task.completed)
+        self.assertIsNone(saved_task.due_at)
 
     def test_is_overdue_future(self):
-        due = timezone.make_aware(datetime(2026, 6, 24, 23, 59, 59))
-        current = timezone.make_aware(datetime(2026, 6, 24, 0, 0, 0))
-        task = Task(title='task1', due_at=due)
-        task.save()
+        due = timezone.make_aware(
+            datetime(2026, 6, 24, 23, 59, 59)
+        )
+        current = timezone.make_aware(
+            datetime(2026, 6, 24, 0, 0, 0)
+        )
+
+        task = Task.objects.create(
+            title='task1',
+            due_at=due
+        )
 
         self.assertFalse(task.is_overdue(current))
 
     def test_is_overdue_past(self):
-        due = timezone.make_aware(datetime(2026, 6, 24, 23, 59, 59))
-        current = timezone.make_aware(datetime(2026, 6, 25, 0, 0, 0))
-        task = Task(title='task1', due_at=due)
-        task.save()
+        due = timezone.make_aware(
+            datetime(2026, 6, 24, 23, 59, 59)
+        )
+        current = timezone.make_aware(
+            datetime(2026, 6, 25, 0, 0, 0)
+        )
+
+        task = Task.objects.create(
+            title='task1',
+            due_at=due
+        )
 
         self.assertTrue(task.is_overdue(current))
 
     def test_is_overdue_none(self):
-        due = None
-        current = timezone.make_aware(datetime(2026, 6, 24, 0, 0, 0))
-        task = Task(title='task1', due_at=due)
-        task.save()
+        current = timezone.make_aware(
+            datetime(2026, 6, 24, 0, 0, 0)
+        )
+
+        task = Task.objects.create(
+            title='task1',
+            due_at=None
+        )
 
         self.assertFalse(task.is_overdue(current))
 
 
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
-        client = Client()
-        response = client.get('/')
+        response = self.client.get(
+            reverse('index')
+        )
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertTemplateUsed(response, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
 
     def test_index_post(self):
-        client = Client()
-        data = {'title': 'Test Task', 'due_at': '2026-06-24 23:59:59'}
-        response = client.post('/', data)
+        data = {
+            'title': 'Test Task',
+            'due_at': '2026-06-24T23:59'
+        }
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 1)
-
-    def test_index_post_random_task(self):
-        client = Client()
-        data = {'action': 'random'}
-        response = client.post('/', data)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 1)
-        self.assertIn(response.context['tasks'][0].title, todo_views.RANDOM_TASK_TITLES)
-
-    def test_index_get_order_post(self):
-        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task1.save()
-        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2026, 8, 1)))
-        task2.save()
-        client = Client()
-        response = client.get('/?order=post')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task2)
-        self.assertEqual(response.context['tasks'][1], task1)
-
-    def test_index_get_order_due(self):
-        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task1.save()
-        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2026, 8, 1)))
-        task2.save()
-        client = Client()
-        response = client.get('/?order=due')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task1)
-        self.assertEqual(response.context['tasks'][1], task2)
-
-    def test_detail_get_success(self):
-        task = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task.save()
-        client = Client()
-        response = client.get('/{}/'.format(task.pk))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/detail.html')
-        self.assertEqual(response.context['task'], task)
-
-    def test_update_get_success(self):
-        task = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task.save()
-        client = Client()
-        response = client.get('/{}/update'.format(task.pk))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name, 'todo/edit.html')
-        self.assertEqual(response.context['task'], task)
-
-    def test_update_post_success(self):
-        task = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task.save()
-        client = Client()
-        data = {'title': 'Updated Task', 'due_at': '2026-07-02 12:30:00'}
-        response = client.post('/{}/update'.format(task.pk), data)
+        response = self.client.post(
+            reverse('index'),
+            data
+        )
 
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('index'))
+        self.assertEqual(Task.objects.count(), 1)
+
+        task = Task.objects.get()
+
+        self.assertEqual(task.title, 'Test Task')
+        self.assertEqual(
+            task.due_at,
+            timezone.make_aware(
+                datetime(2026, 6, 24, 23, 59)
+            )
+        )
+        self.assertFalse(task.completed)
+
+    def test_index_post_without_due_at(self):
+        data = {
+            'title': 'Task Without Due Date',
+            'due_at': ''
+        }
+
+        response = self.client.post(
+            reverse('index'),
+            data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('index'))
+        self.assertEqual(Task.objects.count(), 1)
+
+        task = Task.objects.get()
+
+        self.assertEqual(
+            task.title,
+            'Task Without Due Date'
+        )
+        self.assertIsNone(task.due_at)
+
+    @patch(
+        'todo.views.random.choice',
+        return_value='宇宙人に会う準備をする'
+    )
+    def test_index_post_random_task(self, mock_choice):
+        data = {
+            'action': 'random'
+        }
+
+        response = self.client.post(
+            reverse('index'),
+            data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('index'))
+        self.assertEqual(Task.objects.count(), 1)
+
+        task = Task.objects.get()
+
+        self.assertEqual(
+            task.title,
+            '宇宙人に会う準備をする'
+        )
+        self.assertIsNone(task.due_at)
+
+        mock_choice.assert_called_once()
+
+    def test_index_get_order_post(self):
+        task1 = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        task2 = Task.objects.create(
+            title='task2',
+            due_at=timezone.make_aware(
+                datetime(2026, 8, 1)
+            )
+        )
+
+        response = self.client.get(
+            reverse('index'),
+            {'order': 'post'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/index.html')
+        self.assertEqual(
+            response.context['tasks'][0],
+            task2
+        )
+        self.assertEqual(
+            response.context['tasks'][1],
+            task1
+        )
+
+    def test_index_get_order_due(self):
+        task1 = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        task2 = Task.objects.create(
+            title='task2',
+            due_at=timezone.make_aware(
+                datetime(2026, 8, 1)
+            )
+        )
+
+        response = self.client.get(
+            reverse('index'),
+            {'order': 'due'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/index.html')
+        self.assertEqual(
+            response.context['tasks'][0],
+            task1
+        )
+        self.assertEqual(
+            response.context['tasks'][1],
+            task2
+        )
+
+    def test_detail_get_success(self):
+        task = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        response = self.client.get(
+            reverse('detail', args=[task.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/detail.html')
+        self.assertEqual(
+            response.context['task'],
+            task
+        )
+
+    def test_detail_get_fail(self):
+        response = self.client.get(
+            reverse('detail', args=[999])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_get_success(self):
+        task = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        response = self.client.get(
+            reverse('update', args=[task.pk])
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit.html')
+        self.assertEqual(
+            response.context['task'],
+            task
+        )
+
+    def test_update_post_success(self):
+        task = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        data = {
+            'title': 'Updated Task',
+            'due_at': '2026-07-02T12:30'
+        }
+
+        response = self.client.post(
+            reverse('update', args=[task.pk]),
+            data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url,
+            reverse('detail', args=[task.pk])
+        )
+
         task.refresh_from_db()
-        self.assertEqual(task.title, 'Updated Task')
-        self.assertEqual(task.due_at, timezone.make_aware(datetime(2026, 7, 2, 12, 30, 0)))
+
+        self.assertEqual(
+            task.title,
+            'Updated Task'
+        )
+        self.assertEqual(
+            task.due_at,
+            timezone.make_aware(
+                datetime(2026, 7, 2, 12, 30)
+            )
+        )
+
+    def test_update_post_without_due_at(self):
+        task = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        data = {
+            'title': 'Updated Task',
+            'due_at': ''
+        }
+
+        response = self.client.post(
+            reverse('update', args=[task.pk]),
+            data
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url,
+            reverse('detail', args=[task.pk])
+        )
+
+        task.refresh_from_db()
+
+        self.assertEqual(
+            task.title,
+            'Updated Task'
+        )
+        self.assertIsNone(task.due_at)
 
     def test_update_get_fail(self):
-        client = Client()
-        response = client.get('/999/update')
+        response = self.client.get(
+            reverse('update', args=[999])
+        )
 
         self.assertEqual(response.status_code, 404)
 
     def test_update_post_fail(self):
-        client = Client()
-        data = {'title': 'Updated Task', 'due_at': '2026-07-02 12:30:00'}
-        response = client.post('/999/update', data)
+        data = {
+            'title': 'Updated Task',
+            'due_at': '2026-07-02T12:30'
+        }
+
+        response = self.client.post(
+            reverse('update', args=[999]),
+            data
+        )
 
         self.assertEqual(response.status_code, 404)
 
-    def test_detail_get_fail(self):
-        client = Client()
-        response = client.get('/1/')
+    def test_close_post(self):
+        task = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
 
-        self.assertEqual(response.status_code, 404)
-
-    def test_close_get(self):
-        task = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task.save()
-        client = Client()
-        response = client.get('/{}/close'.format(task.pk))
+        response = self.client.post(
+            reverse('close', args=[task.pk])
+        )
 
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url,
+            reverse('index')
+        )
+
         task.refresh_from_db()
+
         self.assertTrue(task.completed)
 
-    def test_close_get_fail(self):
-        client = Client()
-        response = client.get('/1/close')
-        
-    def test_delete_get_success(self):
-        task = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 7, 1)))
-        task.save()
-        client = Client()
-        response = client.get('/{}/delete/'.format(task.pk))
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/')
-        self.assertFalse(Task.objects.filter(pk=task.pk).exists())
-
-    def test_delete_get_fail(self):
-        client = Client()
-        response = client.get('/1/delete/')
+    def test_close_post_fail(self):
+        response = self.client.post(
+            reverse('close', args=[999])
+        )
 
         self.assertEqual(response.status_code, 404)
+
+    def test_close_get_not_allowed(self):
+        task = Task.objects.create(
+            title='task1'
+        )
+
+        response = self.client.get(
+            reverse('close', args=[task.pk])
+        )
+
+        self.assertEqual(response.status_code, 405)
+
+        task.refresh_from_db()
+
+        self.assertFalse(task.completed)
+
+    def test_delete_post_success(self):
+        task = Task.objects.create(
+            title='task1',
+            due_at=timezone.make_aware(
+                datetime(2026, 7, 1)
+            )
+        )
+
+        response = self.client.post(
+            reverse('delete', args=[task.pk])
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url,
+            reverse('index')
+        )
+        self.assertFalse(
+            Task.objects.filter(pk=task.pk).exists()
+        )
+
+    def test_delete_post_fail(self):
+        response = self.client.post(
+            reverse('delete', args=[999])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_get_not_allowed(self):
+        task = Task.objects.create(
+            title='task1'
+        )
+
+        response = self.client.get(
+            reverse('delete', args=[task.pk])
+        )
+
+        self.assertEqual(response.status_code, 405)
+        self.assertTrue(
+            Task.objects.filter(pk=task.pk).exists()
+        )
